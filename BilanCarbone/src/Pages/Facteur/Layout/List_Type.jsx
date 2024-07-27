ead98ab74d114dd42c68545b484de96c5df06961
unknown <oussamabenrkia0@gmail.com>
2024-07-27 15:38:18 +0100

    type list/desc/acti/softdele

---
+++ BilanCarbone/src/Pages/Facteur/Layout/List_Type.jsx
@@ -0,0 +1,196 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs";
+import { PlusCircle, Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import List_type_all from "./Table/List_type_all";
+import List_Type_Parent from "./Table/List_Type_Parent";
+import List_Type_children from "./Table/List_Type_children";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { API_TYPE } from "@/Api/FacteurApi";
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
+
+const List_Type = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const activeTab = searchParams.get("type") || "tout";
+  const [search, setSearch] = useState("");
+  const [sortConfig, setSortConfig] = useState([]);
+  const [facteurs, setFacteurs] = useState({
+    content: [],
+    number: 0,
+    size: 10,
+    totalElements: 0,
+    totalPages: 1,
+  });
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [filterParam, setFilterParam] = useState("");
+
+  const handleAddClick = () => {
+    navigate('/facteur/ajouter');
+  };
+
+  const fetchData = () => {
+    const sortString = sortConfig.map(e => `&sortBy=${e.key}&sortBy=${e.direction}`).join("");
+    setLoading(true);
+    axios.get(`${API_TYPE.Type}?${filterParam}&page=${currentPage}&search=${search}${sortString}`)
+      .then(response => response.data)
+      .then(response => {
+        setFacteurs(response);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+        showToast('error', 'Problème de chargement des données');
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [currentPage, search, sortConfig, filterParam]);
+
+  const showToast = (type, message) => {
+    const currentdate = new Date();
+    toast[type](message, {
+      description: `${currentdate.getDate()}/${currentdate.getMonth() + 1}/${currentdate.getFullYear()} - ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`,
+    });
+  };
+
+  const handleSortRequest = key => {
+    setSortConfig(prevConfig => {
+      const existingConfigIndex = prevConfig.findIndex(e => e.key === key);
+      const newConfig = [...prevConfig];
+      if (existingConfigIndex !== -1) {
+        newConfig[existingConfigIndex] = { key, direction: newConfig[existingConfigIndex].direction === 'asc' ? 'desc' : 'asc' };
+      } else {
+        newConfig.push({ key, direction: 'asc' });
+      }
+      return newConfig;
+    });
+  };
+
+  const handleDelete = (id, name) => {
+    axios.delete(`${API_TYPE.Type}/${id}`)
+      .then(response => response.data)
+      .then(response => {
+        showToast('success', `Le type ${response.nom_type} a été supprimé`);
+        fetchData();
+      })
+      .catch(error => {
+        console.error("Error deleting data:", error);
+        showToast('error', `Problème de suppression de type ${name}`);
+      });
+  };
+
+  const handleActivate = (id, name, txt) => {
+    axios.put(`${API_TYPE.Type}/${id}/activate?${txt}`)
+      .then(response => response.data)
+      .then(response => {
+        showToast('success', `Le type ${response.nom_type} a été activé`);
+        fetchData();
+      })
+      .catch(error => {
+        console.error("Error activating data:", error);
+        showToast('error', `Problème d'activation de type ${name}`);
+      });
+  };
+
+  const handleDeactivate = (id, name) => {
+    axios.put(`${API_TYPE.Type}/${id}/desactivate`)
+      .then(response => response.data)
+      .then(response => {
+        showToast('success', `Le type ${response.nom_type} a été désactivé`);
+        fetchData();
+      })
+      .catch(error => {
+        console.error("Error deactivating data:", error);
+        showToast('error', `Problème de désactivation de type ${name}`);
+      });
+  };
+
+  return (
+    <main className="mt-4">
+      <Tabs defaultValue={activeTab}>
+        <div className="flex items-center mb-4">
+          <TabsList>
+            <TabsTrigger onClick={() => { setFilterParam(""); setSearchParams({ type: "tout" }) }} value="tout">Tout</TabsTrigger>
+            <TabsTrigger onClick={() => { setFilterParam("parent=true"); setSearchParams({ type: "parent" }) }} value="parent">Parent</TabsTrigger>
+            <TabsTrigger onClick={() => { setFilterParam("detail=true"); setSearchParams({ type: "hierarchie" }) }} value="hierarchie">Hierarchie</TabsTrigger>
+          </TabsList>
+          <div className="relative ml-auto flex items-center gap-2">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Recherche..."
+              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <div className="relative ml-4 flex items-center gap-2">
+            <Button onClick={handleAddClick} size="sm">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Ajouter Type a Facteur</span>
+            </Button>
+          </div>
+        </div>
+
+        <TabsContent value="tout">
+          <div className="text-lg font-semibold text-sapphire-700 m-3">Les types Globaux</div>
+          <List_type_all
+            data={facteurs}
+            loading={loading}
+            sortConfig={sortConfig}
+            requestSort={handleSortRequest}
+            handlePageChange={setCurrentPage}
+            currentPage={currentPage}
+            setSortConfig={setSortConfig}
+            handleDelete={handleDelete}
+            handleActivate={handleActivate}
+            handleDeactivate={handleDeactivate}
+          />
+        </TabsContent>
+        <TabsContent value="parent">
+          <div className="text-lg font-semibold text-sapphire-700 m-3">Les Parents globaux</div>
+          <List_Type_Parent
+            data={facteurs}
+            loading={loading}
+            sortConfig={sortConfig}
+            requestSort={handleSortRequest}
+            handlePageChange={setCurrentPage}
+            currentPage={currentPage}
+            setSortConfig={setSortConfig}
+            handleDelete={handleDelete}
+            handleActivate={handleActivate}
+            handleDeactivate={handleDeactivate}
+          />
+        </TabsContent>
+        <TabsContent value="hierarchie">
+          <div className="text-lg font-semibold text-sapphire-700 m-3">Les types globaux avec des enfants</div>
+          <List_Type_children
+            data={facteurs}
+            loading={loading}
+            sortConfig={sortConfig}
+            requestSort={handleSortRequest}
+            handlePageChange={setCurrentPage}
+            currentPage={currentPage}
+            setSortConfig={setSortConfig}
+            handleDelete={handleDelete}
+            handleActivate={handleActivate}
+            handleDeactivate={handleDeactivate}
+          />
+        </TabsContent>
+      </Tabs>
+    </main>
+  );
+};
+
+export default List_Type;
---
+++ BilanCarbone/src/Pages/Facteur/Layout/Table/List_type_all.jsx
@@ -0,0 +1,230 @@
+import React, { useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  MoreHorizontal, ChevronDown, CircleX, ChevronUp
+} from "lucide-react";
+import {
+  CardFooter,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationLink,
+  PaginationNext,
+} from "@/components/ui/pagination";
+import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
+
+const ListTypeAll = ({ 
+  data, 
+  loading, 
+  sortConfig, 
+  requestSort, 
+  handlePageChange, 
+  currentPage, 
+  setSortConfig, 
+  handledeletesoft, 
+  handleactivate, 
+  handledesactivate 
+}) => {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [showActivateDialog, setShowActivateDialog] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const deleteSort = (key) => {
+    setSortConfig(prevConfig => prevConfig.filter(e => e.key !== key));
+  };
+  const getIconFor = (key) => {
+    const sortItem = sortConfig.find(e => e.key === key);
+    if (!sortItem) {
+      return null;
+    }
+    return sortItem.direction === 'asc' ? (
+      <>
+        <ChevronUp className="ml-2 h-4 w-4 inline" />
+        <CircleX onClick={() => deleteSort(key)} className="ml-2 h-4 w-4 inline" />
+      </>
+    ) : (
+      <>
+        <ChevronDown className="ml-2 h-4 w-4 inline" />
+        <CircleX onClick={() => deleteSort(key)} className="ml-2 h-4 w-4 inline" />
+      </>
+    );
+  };
+
+  const handleDeleteClick = (item) => {
+    setSelectedItem(item);
+    setShowDeleteDialog(true);
+  };
+
+  const handleActivationClick = (item) => {
+    setSelectedItem(item);
+    setShowActivateDialog(true);
+  };
+
+  const confirmDelete = () => {
+    if (selectedItem) {
+      handledeletesoft(selectedItem.id, selectedItem.nom_facteur);
+    }
+    setShowDeleteDialog(false);
+    setSelectedItem(null);
+  };
+
+  const confirmActivation = (toggle) => {
+    if (selectedItem) {
+      handleactivate(selectedItem.id, selectedItem.nom_facteur, toggle ? "all=true" : "");
+    }
+    setShowActivateDialog(false);
+    setSelectedItem(null);
+  };
+
+  return (
+    <Card className="w-full">
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className=" text-center	cursor-pointer w-1/6"><span onClick={() => requestSort('name')}>Nom</span> {getIconFor('name')}</TableHead>
+              <TableHead className="text-center	cursor-pointer hidden md:table-cell w-1/6">Type</TableHead>
+              <TableHead className="text-center	cursor-pointer hidden md:table-cell  w-1/6">Parent</TableHead>
+              <TableHead className="text-center	cursor-pointer hidden md:table-cell  w-1/6">Nomber de facteur</TableHead>
+              <TableHead className="text-center	cursor-pointer hidden md:table-cell  w-1/6"><span onClick={() => requestSort('active')}>Activate</span> {getIconFor('active')}</TableHead>
+              <TableHead className="text-center	cursor-pointer hidden md:table-cell w-1/4"><span onClick={() => requestSort('createdDate')}>Date</span> {getIconFor('createdDate')}</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {loading ? (
+              Array.from({ length: 2 }).map((_, index) => (
+                <TableRow key={index}>
+                  {Array.from({ length: 7 }).map((_, cellIndex) => (
+                    <TableCell key={cellIndex}>
+                      <Skeleton className="h-4 w-[70px] bg-slate-400 mx-auto" />
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : data.content && data.content.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan="7" className="text-2xl text-center py-4">
+                  Pas de Facteurs
+                </TableCell>
+              </TableRow>
+            ) : (
+              data.content.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell className="text-center	font-medium">{item.nom_type}</TableCell>
+                  <TableCell className="text-center	hidden md:table-cell">
+                    <Badge variant="secondary" className={!item.parent ? "bg-slate-400" : "bg-emerald-200"}>
+                      {!item.parent ? "Parent" : "enfant"}
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="text-center	font-medium">{item.type_parent}</TableCell>
+                  <TableCell className="text-center	hidden md:table-cell">{item.nbr_facteur}</TableCell>
+                  <TableCell className="text-center	hidden sm:table-cell">
+                    <Badge variant="outline" className={item.active ? "bg-green-600 text-white" : "bg-red-600 text-white"}>
+                      {item.active ? "Activer" : "Désactiver"}
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="text-center	hidden md:table-cell">{item.create}</TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button aria-haspopup="true" size="icon" variant="ghost">
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end" className="w-48">
+                        <DropdownMenuItem className="text-blue-600">Afficher</DropdownMenuItem>
+                        <DropdownMenuItem className="text-orange-600">Modifier</DropdownMenuItem>
+                        {item.active ? (
+                          <DropdownMenuItem onClick={() => handledesactivate(item.id, item.nom_facteur)} className="text-red-950">Désactiver</DropdownMenuItem>
+                        ) : (
+                          <DropdownMenuItem onClick={() => handleActivationClick(item)} className="text-green-600">Activer</DropdownMenuItem>
+                        )}
+                        <DropdownMenuItem onClick={() => handleDeleteClick(item)} className="text-red-600">Supprimer</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </CardContent>
+      <CardFooter className="flex items-center justify-between">
+        {loading ? (
+          <Skeleton className="h-4 w-full bg-slate-200" />
+        ) : (
+          <>
+            <div className="text-xs text-muted-foreground">
+              Affichage <strong>{data.number * data.size + 1}-{Math.min((data.number + 1) * data.size, data.totalElements)}</strong> de <strong>{data.totalElements}</strong> Facteurs
+            </div>
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious href="#" onClick={() => handlePageChange(Math.max(currentPage - 1, 0))} />
+                </PaginationItem>
+                {Array.from({ length: data.totalPages }).map((_, pageIndex) => (
+                  <PaginationItem key={pageIndex}>
+                    <PaginationLink href="#" onClick={() => handlePageChange(pageIndex)}>{pageIndex + 1}</PaginationLink>
+                  </PaginationItem>
+                ))}
+                <PaginationItem>
+                  <PaginationNext href="#" onClick={() => handlePageChange(Math.min(currentPage + 1, data.totalPages - 1))} />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </>
+        )}
+      </CardFooter>
+      <Dialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
+        <DialogContent>
+          <DialogTitle>Confirmer la suppression</DialogTitle>
+          <DialogDescription>
+            Êtes-vous sûr de bien vouloir supprimer cet élément?    
+          </DialogDescription>
+          <DialogFooter>
+            <Button onClick={() => setShowDeleteDialog(false)}>Annuler</Button>
+            <Button variant="destructive" onClick={confirmDelete}>Supprimer</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={showActivateDialog} onOpenChange={setShowActivateDialog}>
+        <DialogContent>
+          <DialogTitle>Activation</DialogTitle>
+          <DialogDescription>
+            Comment voulez-vous activer?
+          </DialogDescription>
+          <DialogFooter>
+            <Button onClick={() => setShowActivateDialog(false)}>Annuler</Button>
+            <Button className="bg-green-600 text-white hover:bg-green-300 hover:text-black" onClick={() => confirmActivation(0)}>Seulement</Button>
+            <Button className="bg-emerald-900 text-white hover:bg-green-300 hover:text-black" onClick={() => confirmActivation(1)}>Tous avec des enfants</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </Card>
+  );
+};
+
+export default React.memo(ListTypeAll);
