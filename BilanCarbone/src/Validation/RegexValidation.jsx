b5754385a8a1a8c2efdfc962b6097879eed3e853
chalabi hossam <chalabihossam@chalabis-MacBook-Pro.local>
2024-08-08 11:59:31 +0100

    solve the conflit of RegexValidation.jsx

--- BilanCarbone/src/Validation/RegexValidation.jsx
+++ BilanCarbone/src/Validation/RegexValidation.jsx
@@ -16,6 +16,7 @@ export const validate=({username,email,prenom,nom,password,confirmPassword})=>{
     }
     
     if (!PASSWORD_REGEX.test(password)) {
+
       errors.password = 'Mot de passe doit contenir au moins 8 caractères, une lettre et un chiffre et un caractère spèciale.';
     }
     
@@ -27,6 +28,7 @@ export const validate=({username,email,prenom,nom,password,confirmPassword})=>{
   }
 export const validateWithoutPassword=({username,email,prenom,nom})=>{
   const errors={}
+
     if(!USERNAME_REGEX.test(username)){
       errors.username = 'Nom Utilisateur doit être alphanumérique et contenir entre 4 et 15 caractères.';
     }
@@ -37,6 +39,7 @@ export const validateWithoutPassword=({username,email,prenom,nom})=>{
       errors.prenom = 'Prenom doit contenir uniquement des lettres et avoir entre 2 et 20 caractères.';
     }
     if (!NOM_REGEX.test(nom)) {
+
       errors.nom = 'Nom doit contenir uniquement des lettres et avoir entre 2 et 20 caractères.';
     }
     return errors;
