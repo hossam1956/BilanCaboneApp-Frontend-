7ef55f9b1572c4268da4fa2ab5d80f0d4e14fa78
Oussama-benrkia <oussamabenrkia0@gmail.com>
2024-08-09 16:05:58 +0100

    fixed conflit

--- BilanCarbone/src/Composant/ModifyForm.jsx
+++ BilanCarbone/src/Composant/ModifyForm.jsx
@@ -3,6 +3,7 @@ import {validate,validateWithoutPassword} from "@/Validation/RegexValidation";
 import { Eye,EyeOff } from "lucide-react";
 import axios from "axios";
 import { apiClient } from "@/KeycloakConfig/KeycloakConn";
+import Alerts from "@/Composant/Alerts";
 import { Checkbox } from "@/Components/ui/checkbox";
 const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
   const [username, setUsername] = useState(UtilisateurInfo.userRepresentation.username);
@@ -14,9 +15,11 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
   const [errors, setErrors] = useState({});
   const [role,setRole]=useState('MANAGER')
   const [entreprises,setEntreprises]=useState([])
-  const [entreprise,setEntreprise]=useState(entreprises.length > 0 ? entreprises[0].id:1)
+  const [entreprise,setEntreprise]=useState(UtilisateurInfo.entreprise.id)
   const [showPassword,setShowPassword]=useState(false)
   const [updatePaasword,setUpdatePassword]=useState(false)
+  const [alert,setAlert]=useState(false)
+  const [problemAlert,setProblemAlert]=useState(false)
 
   
   useEffect(() => {
@@ -27,7 +30,7 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
       }
       catch(error){
         console.error(error)
-        //setProblemAlert(true);
+        setProblemAlert(true);
       }
       
     };
@@ -72,12 +75,16 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
         email: email,
         firstName: prenom,
         lastName: nom,
-        role : "string",
+        role : role,
+        entreprise_id:entreprise,
         password: "string"
       }
+      console.log(bodyRequest)
+      
       try{
         const update=async()=>{
           const response=await apiClient.put(`/utilisateur?ID=${UtilisateurInfo.userRepresentation.id}`,bodyRequest)
+          setAlert(true)
         }
         update()}
         catch(error){
@@ -88,10 +95,20 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
         }
     }
   }
+  useEffect(() => {
+    if (alert) {
+      const timer = setTimeout(() => {
+        setAlert(false); 
+        window.location.reload();       
+      }, 500);
+
+      return () => {clearTimeout(timer)}
+    }
+  }, [alert]);
 
     return(
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg relative">
+      <div className="bg-white p-6 rounded-lg shadow-lg relative w-1/3 h-fit">
         <button 
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
           onClick={onClose}
@@ -114,9 +131,8 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
                 className={`${errors.username ?"border-red-600 bg-red-300":"border-gray-300 bg-gray-50"} border  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white`}
                 value={UtilisateurInfo.userRepresentation.username}
                 required
-                onChange={(e)=>{setUsername(e.target.value)}}
+                disabled
                />
-               <p className="text-red-500 text-xs text-center">Vous ne pouvez pas changer votre nom utilisateur</p>
                {errors.username && <p className='text-red-500 text-xs text-center'>{errors.username}</p>}
             </div>
             <div>
@@ -134,7 +150,6 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
                 onChange={(e)=>{setEmail(e.target.value)}}
               />
               </div>
-               {errors.email && <p className='text-red-500 text-xs text-center'>{errors.email}</p>}
 
             
             <div>
@@ -195,14 +210,13 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
               <select
                 id="entreprise"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={UtilisateurInfo.entreprise.id}
                 onChange={handleEntrepriseChange}
               >
                 {
                   entreprises.map((entrepriseInfo)=>{
                     const {id,nomEntreprise}=entrepriseInfo
                     return(
-                      <option key={id} value={`${id}`}>{nomEntreprise}</option>
+                      <option key={id} value={`${id}`} selected={id==UtilisateurInfo.entreprise.id} >{nomEntreprise}</option>
                     )
                   })
                   
@@ -269,6 +283,7 @@ const ModifyForm=({onClose,UtilisateurInfo,UtilisateurRole})=>{
 
         </div>
       </div>
+      <Alerts alert={alert} alertProblem={problemAlert} titre_succes={"Utilisateur Modifier"} message_succes={"Utilisateur Modifier avec succès" } message_erreur={"Une erreur est survenue lors de l'envoi de l'opération"}/>
     </div>  
     )
 }
--- BilanCarbone/src/Validation/RegexValidation.jsx
+++ BilanCarbone/src/Validation/RegexValidation.jsx
@@ -1,19 +1,22 @@
+import { USERNAME_REGEX,EMAIL_REGEX,PRENOM_REGEX,NOM_REGEX,PASSWORD_REGEX } from "@/Constants/Regex";
+
 export const validate=({username,email,prenom,nom,password,confirmPassword})=>{
     const errors={}
-    if(!/^[A-Za-z0-9]{4,15}$/.test(username)){
+    if(!USERNAME_REGEX.test(username)){
       errors.username = 'Nom Utilisateur doit être alphanumérique et contenir entre 4 et 15 caractères.';
     }
-    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+    if(!EMAIL_REGEX.test(email)){
       errors.email="Email est invalide"
     }
-    if (!/^[a-zA-Z]{2,20}$/.test(prenom)) {
+    if (!PRENOM_REGEX.test(prenom)) {
       errors.prenom = 'Prenom doit contenir uniquement des lettres et avoir entre 2 et 20 caractères.';
     }
-    if (!/^[a-zA-Z]{2,20}$/.test(nom)) {
+    if (!NOM_REGEX.test(nom)) {
       errors.nom = 'Nom doit contenir uniquement des lettres et avoir entre 2 et 20 caractères.';
     }
     
-    if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)) {
+    if (!PASSWORD_REGEX.test(password)) {
+      
       errors.password = 'Mot de passe doit contenir au moins 8 caractères, une lettre et un chiffre et un caractère spèciale.';
     }
     
@@ -25,16 +28,18 @@ export const validate=({username,email,prenom,nom,password,confirmPassword})=>{
   }
 export const validateWithoutPassword=({username,email,prenom,nom})=>{
   const errors={}
-    if(!/^[A-Za-z0-9]{4,15}$/.test(username)){
+
+    if(!USERNAME_REGEX.test(username)){
       errors.username = 'Nom Utilisateur doit être alphanumérique et contenir entre 4 et 15 caractères.';
     }
-    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+    if(!EMAIL_REGEX.test(email)){
       errors.email="Email est invalide"
     }
-    if (!/^[a-zA-Z]{2,20}$/.test(prenom)) {
+    if (!PRENOM_REGEX.test(prenom)) {
       errors.prenom = 'Prenom doit contenir uniquement des lettres et avoir entre 2 et 20 caractères.';
     }
-    if (!/^[a-zA-Z]{2,20}$/.test(nom)) {
+    if (!NOM_REGEX.test(nom)) {
+
       errors.nom = 'Nom doit contenir uniquement des lettres et avoir entre 2 et 20 caractères.';
     }
     return errors;
