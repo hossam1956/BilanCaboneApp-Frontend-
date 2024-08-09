a44eeccf5fe6002928aa6d3b94f2cbc68879da78
Oussama-benrkia <oussamabenrkia0@gmail.com>
2024-08-09 16:18:14 +0100

    fixed bug

---
+++ BilanCarbone/src/Constants/Regex.jsx
@@ -0,0 +1,9 @@
+export const USERNAME_REGEX = /^[A-Za-z0-9]{4,15}$/;
+
+export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const PRENOM_REGEX = /^[a-zA-Z]{2,20}$/;
+
+export const NOM_REGEX = /^[a-zA-Z]{2,20}$/;
+
+export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
