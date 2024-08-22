fffa6ce6cd951918f5de1a55cc1d736cb4307349
othmanesed11 <othmaneseddik110@gmail.com>
2024-08-22 13:40:06 +0100

    main dash

--- BilanCarbone/src/Static/Main.jsx
+++ BilanCarbone/src/Static/Main.jsx
@@ -1,20 +1,29 @@
-import Navbar from './Navbar';
-import Navheader from './Navheader';
-import { Outlet } from 'react-router-dom';
-import { Toaster } from 'sonner';
+import Navbar from "@/Static/Navbar";
+import Navheader  from "@/Static/Navheader";
+import { Outlet } from "react-router-dom";
+import { Toaster } from 'sonner'
+import SearchProvider from "./SearchProvider";
+import NavbarEmployeResponsable from "./NavbarEmployeResponsable";
 
 const Main = () => {
   return (
+    <SearchProvider>
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      {(sessionStorage.getItem("roleUser")=="ADMIN" || sessionStorage.getItem("roleUser")=="MANAGER")?
       <Navbar />
+      :
+      <NavbarEmployeResponsable/>
+      }
+      
       <div className="flex flex-col">
         <Navheader />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           <Outlet />
         </main>
       </div>
-      <Toaster richColors closeButton />
-    </div>
+      <Toaster richColors closeButton  />
+      </div>
+    </SearchProvider>
   );
 };
 
